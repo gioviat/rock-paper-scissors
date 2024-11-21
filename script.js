7104e95c5c9835a0f7a74998ee43a77c40ad800d
gioviat <gioabati@gmail.com>
2024-11-21 11:00:15 +0100

    Write the logic to play a single round

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     let rand = Math.random();
     if (rand < 1. / 3.) {
@@ -20,3 +17,41 @@ function getHumanChoice() {
 
     return choice;
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`Tie! You both chose ${humanChoice}`);
+    } else if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            console.log("You win! Rock beats Scissors");
+            humanScore++;
+        } else {
+            console.log("You lose! Paper beats Rock");
+            computerScore++;
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win! Paper beats Rock!");
+            humanScore++;
+        } else {
+            console.log("You lose! Scissors beat Paper!");
+            computerScore++;
+        }
+    } else {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats Scissors!");
+            computerScore++;
+        } else {
+            console.log("You win! Scissors beat Paper!");
+            humanScore++;
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
